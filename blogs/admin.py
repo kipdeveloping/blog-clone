811279d558b8e0db6925025913f54b78210bf58a
fernandajien1 <fernandajien01@gmail.com>
2026-08-01 10:21:50 -0500

    blog model done

---
+++ blogs/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at' , 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('author__username', 'created_at')
+
+admin.site.register(Post, PostAdmin)
--- django_base/settings.py
+++ django_base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
