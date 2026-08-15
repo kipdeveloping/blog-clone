c4225ee80fb79cba3b5b2dd0ff05e7043ff1e4d5
kipdeveloping <kipdeveloping@gmail.com>
2026-08-15 10:09:07 -0400

    update done

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post_update')
 ]
